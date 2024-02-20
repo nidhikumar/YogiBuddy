69364bd21eddf43a389428723ae3de10ab10d347
Nidhi Kumar <nidhikr1401@gmail.com>
2024-02-20 13:41:32 -0800

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import './global.css'
+import { Route, Routes } from 'react-router-dom'
+import SignInForm from './_auth/forms/SignInForm'
+import { Home } from './_root/pages'
+import SignUpForm from './_auth/forms/SignUpForm'
+import AuthLayout from './_auth/AuthLayout'
+import RootLayout from './_root/RootLayout'
 
 const App = () => {
   return (
-    <div><h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1></div>
+    <main className='flex h-screen'>
+        <Routes>
+            {/* public routes*/ }
+            <Route element={<AuthLayout />}>
+                <Route path='/sign-in' element={<SignInForm/>}/>
+                <Route path='/sign-up' element={<SignUpForm/>}/> 
+            </Route>
+            {/* private routes*/ }
+            <Route element={<RootLayout/>}>
+                <Route index element={<Home/>}/>
+            </Route>
+        </Routes>
+    </main>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,7 @@ import App from "./App";
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App/>
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
 )
